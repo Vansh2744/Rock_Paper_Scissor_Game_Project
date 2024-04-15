6c9d7de8d116924dcc41386a29ba9269d26272e4
Vansh <118882606+Vansh2744@users.noreply.github.com>
2024-04-15 11:09:03 +0530

    updated files

--- app1.js
+++ app1.js
@@ -101,27 +101,3 @@ choices.forEach((choice) => {
         playGame(userChoice,compChoice);
     })
 });
-//--------------------------------------------------
-/*
-const img=document.querySelector('.logo');
-const play=document.querySelector('.play');
-setInterval(()=>{
-    img.style.transform="scale(1.4)";
-    img.style.transition="all 2s";
-},1000);
-setInterval(()=>{
-    img.style.transform="scale(1)";
-    img.style.transition="all 2s";
-},2000);
-
-
-
-setInterval(()=>{
-    play.style.transform="scale(1.2)";
-    play.style.transition="all 2s";
-},1000);
-setInterval(()=>{
-    play.style.transform="scale(1)";
-    play.style.transition="all 2s";
-},2000);
-*/
